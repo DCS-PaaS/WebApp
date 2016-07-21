db485f89a70ba77472f7da941757c36a3e7d7f71
Grant Weston <grant_weston@dell.com>
2016-07-21 13:43:22 -0500

    Added Commit Line

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -3,5 +3,6 @@ package cc.engeld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, I'm using Travis-CI!");
+		System.out.println("This is a commit");
     }
 }
